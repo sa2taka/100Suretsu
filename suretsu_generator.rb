8ad1bf495def22a5ecaa025da18af4ebe65103c5
sa2taka <sa2taka@gmail.com>
2019-07-30 17:30:20 +0900

    :sparkles: 数列ジェネレーターを追加

---
+++ suretsu_generator.rb
@@ -0,0 +1,12 @@
+class SuretsuGenerator
+    def initialize(ratio, first)
+        @ratio = ratio
+        @first = first
+    end
+
+    def generate(index = 0, count = 10)
+        (index..(index + count - 1)).map do |i|
+            @first * @ratio ** i
+        end
+    end
+end
