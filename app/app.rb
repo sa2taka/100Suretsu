87b5119d35c9da1f9603fe2742db1e8b334d8384
sa2taka <sa2taka@gmail.com>
2019-07-31 15:48:00 +0900

    :sparkles: controller側にロジックを追加

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,36 @@
 require 'rubygems'
 require 'sinatra/base'
+require './suretsu_generator.rb'
+require './lucas_number.rb'
+
+LUCA = LucasNumber.new
 
 class Suretsu < Sinatra::Base
   get '/' do
     'Hello, Sinatra'
+  end
+
+  get '/:index' do
+    if LUCA.index(params[:index].to_i) == 99
+      'congraturate'
+    elsif (@now = LUCA.index(params[:index].to_i))
+      @next = LUCA[@now + 1]
+      @seq, a, r = generate_new_suretsu
+      @seq.to_s + " = #{a} * #{r}^n"
+    else
+      'husei'
+    end
+  end
+
+  private 
+  def generate_new_suretsu
+    a = Random.rand(1..50)
+    r = 0
+    loop do
+      r = Random.rand(3..10)
+      break if a % r != 0
+    end
+    suretsu = SuretsuGenerator.new(r, a)
+    [suretsu.generate(3, 5), a, r]
   end
 end
